ebac9048ec1831db440cda2b0389073de6ea072c
sanderpa <parfeniuksasha09@gmail.com>
2022-03-24 00:52:10 +0200

    <Feat>: Create basic layout. Added bootstrap and routing library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Styles/main.css";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Shelters from "./Pages/Shelters";
+import Rules from "./Pages/Rules";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/shelters" element={<Shelters />} />
+          <Route path="/rules" element={<Rules />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Shelters.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Shelters = () => {
+  return <div>Shelters</div>;
+};
+
+export default Shelters;
